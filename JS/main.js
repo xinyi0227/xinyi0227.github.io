dbd019239105318bdd784e3989ff3323f551e99f
xinyi0227 <1221200909@student.mmu.edu.my>
2024-04-14 03:19:03 +0800

    done

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,28 @@
-// script.js
+// Function to handle form submission
+async function handleFormSubmit(event) {
+    event.preventDefault();
+    const locationInput = document.getElementById('location').value;
+
+    try {
+        const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${locationInput}&limit=1&appid=4434eafdf444c726a6c19b494ca335f8`);
+        const data = await response.json();
+        console.log(data); // Log the data to the console to see its structure
+
+        if (data.length > 0) {
+            const { lat, lon } = data[0];
+            getWeather(lat, lon);
+        } else {
+            console.log('Location not found');
+        }
+    } catch (error) {
+        console.log('Error fetching location data:', error);
+    }
+}
 
 // Function to fetch weather data from the API
 async function getWeather(lat, lon) {
-    const API_KEY = '7a4b2272c6e270912811f8853b05c3a7'; // Replace 'YOUR_API_KEY' with your actual API key
-    const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${API_KEY}`;
+    const API_KEY = '4434eafdf444c726a6c19b494ca335f8'; // Replace 'YOUR_API_KEY' with your actual API key
+    const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely&appid=${API_KEY}`;
 
     try {
         const response = await fetch(apiUrl);
@@ -20,29 +39,6 @@ async function getWeather(lat, lon) {
         `;
     } catch (error) {
         console.log('Error fetching weather data:', error);
-    }
-}
-
-
-
-// Function to handle form submission
-async function handleFormSubmit(event) {
-    event.preventDefault();
-    const locationInput = document.getElementById('location').value;
-
-    try {
-        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${locationInput}&limit=1&appid=YOUR_API_KEY`);
-        const data = await response.json();
-        console.log(data); // Log the data to the console to see its structure
-
-        if (data.length > 0) {
-            const { lat, lon } = data[0];
-            getWeather(lat, lon);
-        } else {
-            console.log('Location not found');
-        }
-    } catch (error) {
-        console.log('Error fetching location data:', error);
     }
 }
 
